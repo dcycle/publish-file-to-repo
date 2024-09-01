0e997e2afd4ffbb17003a9d0b1579061867e6967
Albert Albala <albert@mediatribe.net>
2024-09-01 12:04:11 -0400

    Please squash this commit in the pull request

--- scripts/publish-file-to-repo.sh
+++ scripts/publish-file-to-repo.sh
@@ -27,13 +27,14 @@ fi
 # At this point we will have
 # $GIT_REPO: git@github.com:something/something.git
 # $FILE_LOCATION: /var/jenkins_home/workspace/google-sheets-to-csv-public/indicateurs.csv
-# $MY_REPO_DEPLOY_KEY: /path/to/super/private/deploy-key
+# $MY_REPO_DEPLOY_KEY: contents of super private deploy key
 # $MY_REPO_LOCATION: ./indicateurs
 
 rm -rf unversioned
 mkdir unversioned
 cd unversioned
-ssh-agent bash -c "ssh-add $MY_REPO_DEPLOY_KEY; git clone $GIT_REPO my-repo"
+# https://stackoverflow.com/a/46253163
+ssh-agent bash -c "ssh-add - <<< ${MY_REPO_DEPLOY_KEY}; git clone $GIT_REPO my-repo"
 cd my-repo
 mkdir -p $MY_REPO_LOCATION
 cp $FILE_LOCATION $MY_REPO_LOCATION
